e02ed5751fd7572a2a2833114d863700af9390df
UtenLapanikorn <vasana2547a@gmail.com>
2025-05-22 09:56:07 +0700

    1

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,18 +1,11 @@
 'use server';
 
-import { prisma } from "@/lib/prisma";
+import { prisma } from '@/lib/prisma';
 
-export async function login(username:string, password:string) {
-    const user = await prisma.user.findFirst({where: { usernsme: username}});
-       
-    if(user){
-        if(user.password === password) {
-            return true
-        } else {
-            return false
-        }
-    } else {
-        return false
-    }
+export async function login(username: string, password: string) {
+  const user = await prisma.user.findUnique({
+    where: { username },
+  });
 
+  return user?.password === password;
 }
